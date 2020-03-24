b2c66de03890a3d6e7883a92a80c193b42d0aab8
codenameuriel <u.e.rodriguez@gmail.com>
2020-03-24 16:27:10 -0400

    Done.

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,16 +1,43 @@
 class RecipesController < ApplicationController
-  def show
-    @recipe = Recipe.find(params[:id])
-  end
-
   def index
     @recipes = Recipe.all
   end
 
+  def show
+    @recipe = Recipe.find(params[:id])
+  end
+
   def new
     @recipe = Recipe.new
+    2.times { @recipe.ingredients.build }
   end
 
   def create
+    @recipe = Recipe.new(recipe_params)
+    if @recipe.save 
+      redirect_to @recipe
+    else
+      render :new 
+    end
+  end
+
+  private 
+
+  def recipe_params
+    params.require(:recipe).permit(:title, ingredients_attributes: [:name, :quantity])
   end
 end
+
+# "recipe"=>{
+#   "title"=>"Chicken Terkiyaki", 
+#   "ingredients_attributes"=>{
+#     "0"=>{
+#       "name"=>"Chicken", 
+#       "quantity"=>"1 lb"
+#       }, 
+#       "1"=>{
+#         "name"=>"Teriyaki Sauce", 
+#         "quantity"=>"2 cups"
+#         }
+#         }
+#       }
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,3 +1,32 @@
 class Recipe < ActiveRecord::Base
-  has_many :ingredients 
+  has_many :ingredients
+  accepts_nested_attributes_for :ingredients
 end
+
+# Instructions
+# Build a recipe form that accepts two ingredients! It should automatically create the new Ingredient objects
+# Ingredient has a name and quantity (String values)
+
+# accepts_nested_attributes_for macro is creating implicitly
+def ingredients_attributes=(ingredients_hash)
+  ingredients_hash.each do |i|
+    ingredient = Ingredient.find_or_create_by(name: i[:name], quantity: i[:quantity])
+    self.ingredients << ingredient
+  end
+end
+# ***can not override the implicitly created method by the accepts_nested_attributes_for macro
+# ***if trying to avoid duplicate entries, cannot include macro, and have to explicitly define method (and use #update on the found/created object)
+
+# "recipe"=>{
+#   "title"=>"Chicken Terkiyaki", 
+#   "ingredients_attributes"=>{
+#     "0"=>{
+#       "name"=>"Chicken", 
+#       "quantity"=>"1 lb"
+#       }, 
+#       "1"=>{
+#         "name"=>"Teriyaki Sauce", 
+#         "quantity"=>"2 cups"
+#         }
+#         }
+#       }
